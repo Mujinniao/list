0400c2a450dd863dd4e33bb5af202be331410c58
Anekonata <Anekonata@example.com>
2026-07-30 21:33:29 +0800

    up

--- mod.sh
+++ mod.sh
@@ -1,82 +1,64 @@
 #!/bin/bash
 
 
-curl -o abp1.txt https://raw.githubusercontent.com/easylist/easylist/gh-pages/easylist.txt
-curl -o abp2.txt https://raw.githubusercontent.com/easylist/easylist/gh-pages/easyprivacy.txt
-curl -o abp3.txt https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt
-curl -o abp4.txt https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt
-curl -o abp5.txt https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-ublock.txt
-curl -o abp6.txt https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt
-curl -o abp7.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt
-curl -o abp8.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
-curl -o abp9.txt https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt
-curl -o abp10.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
-# curl -o abp11.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/filters.min.txt
-# curl -o abp12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/privacy.min.txt
-# curl -o abp13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/badware.txt
-# curl -o abp14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/quick-fixes.txt
-# curl -o abp15.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/unbreak.txt
-curl -o abp16.txt https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/nocoin.txt
-curl -o abp17.txt https://raw.githubusercontent.com/durablenapkin/scamblocklist/master/adguard.txt
-curl -o abp18.txt https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt
-# curl -o abp19.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/BaseFilter/sections/adservers.txt
-# curl -o abp20.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/BaseFilter/sections/adservers_firstparty.txt
-# curl -o abp21.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/BaseFilter/sections/foreign.txt
-# curl -o abp22.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/MobileFilter/sections/adservers.txt
-# curl -o abp23.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/adservers.txt
-# curl -o abp24.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/adservers_firstparty.txt
 
-curl -o adg1.txt https://raw.githubusercontent.com/ilpl/ad-hosts/master/hosts
-curl -o adg2.txt https://raw.githubusercontent.com/ilpl/ad-hosts/master/filter.txt
-curl -o adg3.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ublock-dynamic-rule.txt
-
-
-#curl -o adg3.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
-#curl -o adg4.txt https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
-#curl -o adg5.txt https://raw.githubusercontent.com/francis-zhao/quarklist/master/dist/hosts
-#curl -o abp2.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
-#curl -o abp3.txt https://raw.githubusercontent.com/francis-zhao/quarklist/master/dist/quarklist.txt
+curl -o abp1.txt https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt
+curl -o abp2.txt https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt
+curl -o abp3.txt https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/adblock.txt
+curl -o abp4.txt https://raw.githubusercontent.com/lingeringsound/adblock_auto/main/Rules/adblock_auto.txt
+curl -o adg1.txt https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/all.txt
 
 
 
+# 预处理并合并所有源文件
+sed -i -e '/^#[^#]/d' -e '/^####/d' -e '/^### /d' -e '/^$/d' adg1.txt 
+cat adg1.txt abp*.txt | sed -e '/^!/d' -e '/^# /d' -e 's/^\s*//' -e 's/\s*$//' -e 's/\r//g' -e '/^$/d' | sort -u > adall.tmp
 
+# 处理例外规则
 
+grep '^@@' adall.tmp | sed 's/^@@//' > except.tmp
+grep -v '^@@' adall.tmp | grep -v -F -f except.tmp > adall.tmp2
 
+# 处理 hosts 格式并转换
+grep -e '^0.0.0.0' -e '^127.0.0.1' adall.tmp2 | sed -e 's/^[0-9.]* //' -e 's/^/||/' -e 's/$/^/' > hosts.tmp
+grep -v -e '^0.0.0.0' -e '^127.0.0.1' adall.tmp2 > adall.tmp3
 
-cat adg*.txt | sort -u | sed -e 's/#.*//' -e '/^$/d' > adgm.txt
+# 合并所有规则
+cat hosts.tmp adall.tmp3 | sort -u > tmp && mv tmp adall.txt
 
-cat abp*.txt | sort -u | sed -e '/^\[/d' -e '/^!/d' -e '/^$/d' > abpm.txt  
+# 提取和过滤包含特殊字符的规则
+awk '/^\|\|/ && !/[\/^$*]/' adall.txt > adall.tmp4
+awk '!/^\|\|/ || /[\/^$*]/' adall.txt > tmp && mv tmp adall.txt
+awk '!/^\|\|[0-9.]+$/ || split($0, a, ".") < 4' adall.tmp4 | sed 's/$/^/' > adall.tmp5
 
-cat adgm.txt abpm.txt | sort -u > adall.txt
+# 合并
+cat adall.tmp5 adall.txt | sort -u > tmp && mv tmp adall.txt
 
-grep '^@@' adall.txt > white.txt && sed -i -e '/^@@/d' -e '/^$/d' adall.txt
+# 处理 HTTP/HTTPS 规则
+grep -e '^||https:' -e '^||http:' adall.txt > adall.tmp6
+sed -i '/^||https:/d;/^||http:/d' adall.txt
+sed -e 's/||https:\/\//||/' -e 's/||http:\/\//||/' -e '/#/d' adall.tmp6 | awk '!/[/$]/ { $0 = $0 "^" } 1' > adall.tmp7
 
-sed -i -e 's/\$important$//' -e 's/^@@//' -e 's/^||/|/' -e 's/^|/||/' white.txt
+# 合并
+cat adall.txt adall.tmp7 | sort -u > tmp && mv tmp adall.txt
 
-# sort adall.txt -o adall.txt
+# 处理无效规则
+cat adall.txt | \
+  sed -E 's/^\|\|0\.0\.0\.0\./||/' | \
+  sed -E 's/^\|\|127\.0\.0\.1\./||/' | \
+  grep -v -E '^\|\|(0\.0\.0\.[0-9]+|127\.0\.0\.1|::1|\[::\]|localhost|localhost\.localdomain|ip6-localhost|ip6-loopback)\^' | \
+  grep -v -E '^\|\|[^\^]*\s+[^\^]*\^' | \
+  grep -v '^::1' | \
+  sed -e 's/^\s*//' -e 's/\s*$//' | \
+  grep -v '^$' | \
+  sort -u > tmp && mv tmp adall.txt
 
-# sort white.txt -o white.txt
+grep -E '^\|\|[a-zA-Z0-9.-]+\^$' adall.txt > damain.txt
 
-grep -v -F -f white.txt adall.txt > temp.txt && mv temp.txt adall.txt
+grep -v -E '^\|\|[a-zA-Z0-9.-]+\^$' adall.txt > modify.txt
 
-# comm -23 adall.txt white.txt > temp.txt && mv temp.txt adall.txt
-
-grep -e '^||' -e '^0.0.0.0' -e '^127.0.0.1' adall.txt > domain.txt
-
-sed -i -e '/\//d' -e '/\$popup/d' -e '/\$third-party/d' -e '/\$domain/d' -e '/\$redirect/d' -e '/\$script/d' -e '/\$~image/d' -e '/,third-party/d' -e '/\$image/d' -e '/\^\*/d' -e '/\$csp/d' -e '/\$media/d' -e '/\$removeheader/d' -e '/\$subdocument/d' -e '/\$object/d' -e '/\$xhr/d' -e '/\$cookie/d' -e '/\$all/d' -e '/\$xmlhttprequest/d' -e '/\$document/d' -e '/domain=/d' -e '/\$~stylesheet/d' -e '/\$removeparam/d' -e '/\$websocket/d' -e '/\$~third-party/d' -e '/\$inline-script/d' -e '/\$app/d' -e '/\$header/d' -e '/\$match-case/d' -e '/\$method/d' -e '/\$to/d' -e '/^$/d' domain.txt
-
-sort adall.txt -o adall.txt
-
-sort domain.txt -o domain.txt
-
-comm -23 adall.txt domain.txt > modify.txt
-
-rm adg*.txt abp*.txt white.txt
-
-cat ./extra/add.txt >> domain.txt
-
-sed -e '/\*/d' -e 's/0.0.0.0 //' -e 's/127.0.0.1 //' -e 's/||//' -e 's/\^.*//' -e 's/^/domain:/' domain.txt > mos.txt
-
+# 清理临时文件
+rm -f adg1.txt abp*.txt *.tmp* 
 
 
 
